319e471162699b7fd8967811fbeb57fb15392a8e
j_fyfe <jasonfyfe.dev@gmail.com>
2020-09-09 11:03:35 +0100

    Initial Commit

---
+++ exercise.js
@@ -0,0 +1,11 @@
+// Select all divs and give them a purple background
+$("div").css("background-color", "purple");
+
+// Selecte divs with class "highlight" make them 200px wide
+$(".highlight").css("width", "200px");
+
+// Select dive with id "third" and give it an orange border
+$("#third").css("border", "4px solid orange");
+
+// Bonus: Select first div change font to pink
+$("div:first").css("color", "pink");
